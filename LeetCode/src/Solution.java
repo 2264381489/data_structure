ccc96ed25d7b5f21df03ac59d314176428257665
2264381489@qq.com <2264381489@qq.com>
2020-02-27 21:23:29 +0800

    LeetCode26.删除排序数组中的重复项

---
+++ LeetCode/src/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int number = Integer.MAX_VALUE;
+        int flag = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != number) {
+                number = nums[i];
+                nums[flag] = nums[i];
+                flag++;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(new int[]{-1, 0, 0, 0, 0, 3, 3}));
+    }
+
+//官方题解（比你这个简洁一百倍！！！）
+    public int removeDuplicates1(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
