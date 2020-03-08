e42c443c734b23a453d579b4af61314569c36e54
2264381489@qq.com <2264381489@qq.com>
2020-03-08 15:25:20 +0800

    有序链表

--- Datastructures/src/linkdelist/SingleLinkedListDemo.java
+++ Datastructures/src/linkdelist/SingleLinkedListDemo.java
@@ -10,15 +10,26 @@ public class SingleLinkedListDemo {
         HeroNode heroNode4 = new HeroNode(4, "林冲", "豹子头");
         //创建一个链表
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(heroNode1);
-        singleLinkedList.add(heroNode2);
-        singleLinkedList.add(heroNode3);
-        singleLinkedList.add(heroNode4);
+//        singleLinkedList.add(heroNode1);
+//        singleLinkedList.add(heroNode2);
+//        singleLinkedList.add(heroNode3);
+//        singleLinkedList.add(heroNode4);
+        //加入按照编号的顺序
+        singleLinkedList.addByOrder(heroNode1);
+        singleLinkedList.addByOrder(heroNode4);
+        singleLinkedList.addByOrder(heroNode2);
+        singleLinkedList.addByOrder(heroNode3);
         //显示一把
         singleLinkedList.list();
-
-
-        //加入
+       //测试修改节点的代码
+        HeroNode newheroNode = new HeroNode(2, "小路", "小尾巴");
+singleLinkedList.update(newheroNode);
+        System.out.println("修改后链表");
+        singleLinkedList.list();
+        //删除一个节点
+        System.out.println("删除一个");
+        singleLinkedList.del(4);
+        singleLinkedList.list();
 
     }
 }
@@ -46,6 +57,97 @@ class SingleLinkedList{
         }
         temp.next=heroNode;
     }
+    //第二种方式添加英雄时,根据排名将英雄插入到指定位置
+    //(如果有这个排名,则添加失败,并给出提示.)
+    public void addByOrder(HeroNode heroNode){
+      //因为头节点不能动,因此我们仍然通过一个辅助指针(变量)来帮助找到添加的位置
+        //因为单链表,因为我们找到的temp时位于添加位置的前一个节点,否则加插入不了.
+        HeroNode temp=head;
+        boolean flag=false;//添加的编号是否存在,默认为false
+        //这个循环就是找位置,并不是找到位置直接插入,这也是为什么break,找到位置就跳出
+        while(true){
+            if(temp.next==null){
+                //说明temp已经到了链表的最后了
+                break;
+            }
+            if(temp.next.no>heroNode.no){
+                //位置找到,就到temp的后面插入.因为要按顺序插入,所以找到第一个比要插入节点大的节点.
+                break;
+            }else if(temp.next.no==heroNode.no){
+                flag=true;
+                break;
+
+            }
+            temp=temp.next;
+        }
+        //判断falg的值
+        if(flag){
+            //不能添加,说明编号存在
+            System.out.printf("准备插入的英雄的编号%d 已经存在了,不能加入\n",heroNode.no);
+        }else{
+            heroNode.next=temp.next;//将加入节点的指针指向下一个节点(temp.next是一个值,表示temp下一个节点)
+            temp.next=heroNode;//(temp.next在这里是一个指针)
+        }
+    }
+    //修改节点的信息,根据编号来修改,既no编号不能改
+    //说明
+    //1.根据newHeroNode的no来修改即可
+    public void update(HeroNode newHeroNode){
+        if (head.next==null){
+            System.out.println("链表为空");
+        return;
+        }
+        //找到需要的节点,根据no编号
+        //定义一个辅助变量
+        HeroNode temp=head.next;
+        boolean flag=false;//表示是否找到该节点
+        while(true){
+            if(temp==null){
+                break;//到链表的遍历完毕了.
+            }
+            if(temp.no==newHeroNode.no){
+                //找到了
+                flag=true;
+                break;
+            }
+            temp=temp.next;
+        }
+        //根据flag判断是否找到要修改的节点
+        if (flag){
+            temp.name=newHeroNode.name;
+            temp.nickname=newHeroNode.nickname;
+        }else{
+            //没有找到
+            System.out.printf("没有找到编号等于%d的节点\n",newHeroNode.no);
+        }
+    }
+//删除节点
+    //思路
+    //1.head不能动,因此我们需要一个temp辅助节点找到删除节点的前一个节点.
+    //2.说明我们在比较时,是temp.next.no和需要删除的节点的no比较
+    public void del(int no){
+        HeroNode temp=head;
+        boolean flag=false;//标识是否找到待删除节点的
+        while(true){
+            if(temp.next==null){
+                //已经到了链表的最后
+                break;
+            }
+            if (temp.next.no==no){
+                //找到了待删除节点的前一个结点temp
+                flag=true;
+                break;
+            }
+            temp=temp.next;//temp后移,遍历
+        }
+        //判断flag
+        if(flag){
+            temp.next=temp.next.next;
+        }else{
+            System.out.printf("要删除的%d节点不存在\n",no);
+        }
+    }
+
     //显示链表
     public void list(){
         //先判断链表是否为空
@@ -91,3 +193,4 @@ class HeroNode{
     }
     //为了显示方法，我们重新toString
 }
+
