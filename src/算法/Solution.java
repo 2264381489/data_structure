930521e8f96cf0ff4b05ebc133f118a092384833
2264381489@qq.com <2264381489@qq.com>
2020-03-13 22:20:51 +0800

    失败的算法

--- src/算法/Solution.java
+++ src/算法/Solution.java
@@ -1,48 +1,71 @@
 package 算法;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
+
+//
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//class Solution {
+//    public String gcdOfStrings(String str1, String str2) {
+//        //试一下滑窗
+//        char[] arr1 = str1.toCharArray();
+//        System.out.println(arr1[1]);
+//        char[] arr2 = str2.toCharArray();
+//        int i=1;
+//        int j=0;
+////        while(j<arr1.length&&j<arr2.length){
+////            if(arr1[j]==arr2[j]){
+////                j++;
+////            }else{
+////                return "";
+////            }
+////        }
+//        if(!(str1+str2).equals(str2+str1)){
+//            return "";
+//        }
+//        Queue<Character> queue = new LinkedList<Character>();
+//        char a='a';
+//        int n=0;
+//        queue.offer(arr1[0]);
+//        while(i<arr1.length){
+//
+//            if(arr1[i]==a){
+//                n++;
+//                i++;
+//                continue;
+//            }
+//            a=arr1[i];
+//            if(arr1[i]!=queue.peek()){
+//                queue.offer(arr1[i]);
+//                n++;
+//            }else{
+//                if (arr1[i+1]==queue.peek()){
+//                    n++;
+//                }
+//                break;
+//            }
+//            i++;
+//        }
+//        return str1.substring(0,n+1);
+//    }
+//
+//    public static void main(String[] args) {
+//        Solution solution = new Solution();
+//        System.out.println(solution.gcdOfStrings("OBCNOOBCNO", "OBCNO"));
+//    }
+//}
+////"OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO"
+////        "OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO"
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        //试一下滑窗
-        char[] arr1 = str1.toCharArray();
-        System.out.println(arr1[1]);
-        char[] arr2 = str2.toCharArray();
-        int i=1;
-        int j=0;
-        while(j<arr1.length&&j<arr2.length){
-            if(arr1[j]==arr2[j]){
-                j++;
-            }else{
-                return "";
-            }
-        }
-        Queue<Character> queue = new LinkedList<Character>();
-        char a='a';
-        int n=0;
-        queue.offer(arr1[0]);
-        while(i<arr1.length){
-
-            if(arr1[i]==a){
-                n++;
-                continue;
-            }
-            a=arr1[i];
-            if(arr1[i]!=queue.peek()){
-                queue.offer(arr1[i]);
-                n++;
-            }else{
-                break;
-            }
-            i++;
-        }
-        return str1.substring(0,n+1);
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[Math.round(nums.length/2)];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.gcdOfStrings("LEET",
-                "CODE"));
+        System.out.println(solution.majorityElement(new int[]{2,2,1,1,1,2,2}));
     }
 }
