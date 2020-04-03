98dde74e9291d1710e6e6f7aa8fe714c24f06f99
2264381489@qq.com <2264381489@qq.com>
2020-04-03 23:09:46 +0800

    逆波兰改进

---
+++ src/算法/test.java
@@ -0,0 +1,12 @@
+package 算法;
+
+public class test {
+    public static void main(String[] args) {
+
+int i=1;//没有块级作用域的js变量(var),类似于java中的这个变量.作用域为整个函数
+        for ( i = 1; i < 3; i++) {
+         //注意,
+        }
+        System.out.println(i);
+    }
+}
